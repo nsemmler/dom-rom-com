30c65326c6adeab878c249f9548ca26c18561a15
Nate <ndsunc@gmail.com>
2018-05-24 11:59:43 -0700

    Alternate solutions for looping over table row information (and changing values)

--- scripts/main.js
+++ scripts/main.js
@@ -16,11 +16,29 @@ for (let i = 0; i < rowsArr.length; i++) {
   console.log(rowsArr[i].children[1].textContent)
 }
 
+// ^^ can be solved like so:
+/*
+  const trElements = document.querySelectorAll('tr')
+  const trs = Array.from(trElements)
+
+  trs.forEach((element) => {
+    console.log(element.children[1].textContent)
+  })
+*/
+
 // Change the last <td> of each <tr> to include the count of how many actors are listed
 for (let i = 0; i < rowsArr.length; i++) {
   const starsStr = rowsArr[i].children[3].textContent
   rowsArr[i].children[3].textContent += ` (${starsStr.split(',').length})`
 }
+
+// ^^ can be solved like so:
+/*
+  trs.forEach((element) => {
+    const starringStr = element.children[3].textContent
+    element.children[3].textContent += ` (${starringStr.split(',').length})`
+  })
+*/
 
 // Remove the bg-dark class from the Jumbotron
 const element = document.getElementsByClassName('jumbotron')[0]
