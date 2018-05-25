db0cd3ff576dfa0df1bfecc0c27c0ded52b00a01
Nate <ndsunc@gmail.com>
2018-05-25 10:45:51 -0700

    Created scripts/data.js file containing array of movie objects

---
+++ scripts/data.js
@@ -0,0 +1,11 @@
+const movies = [
+  { title: 'Say Anything...', year: 1989, score: '98%', starring: 'John Cusack, Ione Skye, John Mahoney, Lili Taylor' },
+  { title: 'Groundhog Day', year: 1993, score: '96%', starring: 'Bill Murray, Andie MacDowell, Chris Elliott, Stephen Tobolowsky' },
+  { title: 'Obvious Child', year: 2014, score: '90%', starring: 'Jenny Slate, Gaby Hoffmann, David Cross, Jake Lacy' },
+  { title: 'The Big Sick', year: 2017, score: '98%', starring: 'Kumail Nanjiani, Zoe Kazan, Holly Hunter, Ray Romano' },
+  { title: 'Roman Holiday', year: 1953, score: '98%', starring: 'Audrey Hepburn, Gregory Peck, Eddie Albert, Tullio Carminati' },
+  { title: 'City Lights', year: 1931, score: '98%', starring: 'Charles Chaplin, Virginia Cherrill, Harry Myers, Florence Lee' },
+  { title: 'The Princess Bride', year: 1987, score: '97%', starring: 'Cary Elwes, Robin Wright, Mandy Patinkin, Chris Sarandon' },
+]
+
+module.exports = movies
